b46f3c7f2745cc5b061733cd9c1ac134be06eabe
Garrett Sutula <garrettsutula@gmail.com>
2022-03-27 11:32:17 -0400

    purchase server bugfix

--- src/servers/purchase-server-max.js
+++ src/servers/purchase-server-max.js
@@ -1,13 +1,13 @@
 /** @param {import("..").NS } ns */
 export async function main(ns) {
-    const intervalSeconds = 30 * 1000;
+    const intervalSeconds = 30 * 10;
     ns.disableLog("sleep");
     ns.disableLog("getServerMoneyAvailable");
-    var ram = 1048576; // It's over 9000
+    var ram = Math.floor(1048576); // It's over 9000
 
-    let purchasedServerCount = ns.getPurchasedServers().length - 1;
+    let purchasedServerCount = ns.getPurchasedServers().length;
 
-    while (purchasedServerCount >= ns.getPurchasedServerLimit()) {
+    while (purchasedServerCount <= ns.getPurchasedServerLimit()) {
         if (ns.getServerMoneyAvailable("home") > ns.getPurchasedServerCost(ram)) {
             const hostname = ns.purchaseServer(`gserv-${purchasedServerCount}`, ram);
             ns.tprint(`Purchased server: ${hostname}@${ram/1000/1000}PB`);
