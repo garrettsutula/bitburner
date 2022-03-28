4e03c5161480bb0d250abcf9bd57d914ce9263a5
Garrett Sutula <garrettsutula@gmail.com>
2022-03-27 21:02:50 -0400

    Add route-finder

---
+++ src/discovery/route-finder.js
@@ -0,0 +1,23 @@
+const foundPaths = [];
+const seen = []
+
+/** @param {import("..").NS } ns */
+function recursiveScan(ns, host, targetHost, networkSignature) {
+  networkSignature += `.${host}`;
+  if (host === targetHost) {
+    foundPaths.push(networkSignature);
+  }
+  if (!seen.includes(host)) {
+    seen.push(host);
+    return ns.scan(host).map((childHost) => recursiveScan(ns, childHost, targetHost, networkSignature));
+  }
+}
+
+/** @param {import("..").NS } ns */
+export async function main(ns) {
+  const [ targetHost ] = ns.args;
+  recursiveScan(ns, 'home', targetHost, '');
+  ns.tprint(`Found paths to host:
+  ${foundPaths.join("\n")}
+  `);
+}
